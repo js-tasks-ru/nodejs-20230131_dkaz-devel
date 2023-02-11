831e1c4661ef0466f88ac337d2fb8b740b002556
Denis <denkazarin@icloud.com>
2023-02-11 18:05:43 +0300

    add solution for 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,17 +1,40 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require("fs");
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
+  const url = new URL(req.url, `http://localhost:3000/${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  const filepath = path.join(__dirname, 'test/fixtures', pathname);
+
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('Incorrect path')
+  }
 
   switch (req.method) {
     case 'GET':
+      const stream = fs.createReadStream(filepath);
+
+      stream.on('error', err => {
+        if (err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('File is not found')
+        } else {
+          res.statusCode = 500;
+          res.end('Something went wrong')
+        }
+      })
+
+      stream.pipe(res);
+
+      req.on('aborted', () => {
+        stream.destroy()
+      })
 
       break;
 
@@ -19,6 +42,7 @@ server.on('request', (req, res) => {
       res.statusCode = 501;
       res.end('Not implemented');
   }
+
 });
 
 module.exports = server;
