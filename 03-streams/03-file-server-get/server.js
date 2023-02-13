e446aa04b9a0dbc0e79287376ecdc10670c3cdbb
Denis <denkazarin@icloud.com>
2023-02-13 10:54:37 +0300

    small fix for 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const url = new URL(req.url, `${req.headers.host}`);
+  const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
